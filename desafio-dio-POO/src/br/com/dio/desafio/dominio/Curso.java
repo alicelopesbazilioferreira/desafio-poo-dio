741fc97cc4b3ea62f2a4929fe17837f42635935f
alicelopesbazilioferreira <alicelopesbazilioferreira@gmail.com>
2023-04-09 03:56:03 -0300

    Bootcamp e Dev

--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,24 +1,12 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 	
-	private String titulo;
-	private String descricao;
+	
 	private Integer cargaHoraria;
 	
 	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
 	public Integer getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -26,12 +14,16 @@ public class Curso {
 		this.cargaHoraria = cargaHoraria;
 	}
 	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
+	}
+	
 	
 	@Override
 	public String toString() {
-		return "Curso - titulo: " + titulo + ", descricao: " + descricao + ", cargaHoraria: " + cargaHoraria ;
+		return "Curso - titulo: " + getTitulo() + ", descricao: " + getDescricao() + ", cargaHoraria: " + cargaHoraria ;
 	}
-	
 	
 	
 	
